a7313d1deadf9b54aaa9c2a81d3ad089d296206f
amirova312 <129772929+amirova312@users.noreply.github.com>
2023-04-07 14:32:51 +0600

    Update Dev.java

--- src/main/java/com/digital/Dev.java
+++ src/main/java/com/digital/Dev.java
@@ -3,5 +3,6 @@ package com.digital;
 public class Dev {
     public static void main(String[] args) {
         System.out.println("Dev");
+        System.out.println("Hello");System.out.println("Hello");
     }
 }
