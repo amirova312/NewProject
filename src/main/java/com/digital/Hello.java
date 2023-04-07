2837bac74360c283fede49dd13818abf362a46c5
amirova312 <129772929+amirova312@users.noreply.github.com>
2023-04-07 13:57:02 +0600

    Update Hello.java

--- src/main/java/com/digital/Hello.java
+++ src/main/java/com/digital/Hello.java
@@ -4,5 +4,7 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("new remote branch");
         System.out.println("Hello");
+        
+        System.out.println("Hello");System.out.println("Hello");System.out.println("Hello");
     }
 }
